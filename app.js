4d68f1952c83066eb5e26d5b04506b303ba6a815
dhtlee <dhtlee@rocketmail.com>
2017-04-27 12:42:23 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+const Stars = (props) => {
+  const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+	return (
+    <div className="col-md-5">
+    	{_.range(numberOfStars).map(i =>
+        <i key={i} className="fa fa-star"></i>
+      )}
+    </div>
+  )
+}
+
+const Button = (props) => {
+	return (
+    <div className="col-md-2">
+    	<button>=</button>
+    </div>
+  )
+}
+
+const Answer = (props) => {
+	return (
+    <div className="col-md-5">
+    	{ props.selectedNumbers.map((number, i) =>
+        <span key={i}>{number}</span>
+      )}
+    </div>
+  )
+}
+
+const Numbers = (props) => {
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  }
+  return (
+    <div className="card text-center">
+      <div>
+        { Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
+Numbers.list = _.range(1, 10);
+
+const Number = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedNumbers: []
+    }
+    this.selectNumber = this.selectNumber.bind(this);
+  }
+
+  selectNumber(clickedNumber) {
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+    }))
+  }
+
+  render() {
+    return (
+      <div className="container">
+      	<h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer selectedNumbers={this.state.selectedNumbers} />
+        </div>
+        <br />
+        <Numbers selectedNumbers={this.state.selectedNumbers}
+                  selectNumber={this.selectNumber} />
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+	render() {
+  	return (
+    	<div>
+      	<Game />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
